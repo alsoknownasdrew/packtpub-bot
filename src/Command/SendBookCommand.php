c90ed0a3c3f03e57e97519b681052b988815a68f
alsoknownasdrew <andriy.frankevych@gmail.com>
2019-04-06 19:22:12 +0200

    Add base Symfony Command

---
+++ src/Command/SendBookCommand.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PPBot\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class SendBookCommand
+ *
+ * @package PPBot\Command
+ */
+class SendBookCommand extends Command
+{
+    protected function configure(): void
+    {
+        $this->setName('send-book')
+            ->setDescription('Sends the latest Packt Publishing Free eBbook to specified in .env Slack channel.')
+            ->setHelp('Sends the latest Packt Publishing Free eBbook to specified in .env Slack channel.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): void
+    {
+        $output->writeln('Hello world!');
+    }
+}
