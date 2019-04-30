5118a467fceb6c89e54de94889cc37d9e3041794
alsoknownasdrew <andriy.frankevych@gmail.com>
2019-04-30 17:58:44 +0200

    Move Application initialization to service container

--- config/container.php
+++ config/container.php
@@ -8,10 +8,14 @@ $container = new Container;
 
 $config = require __DIR__ . '/config.php';
 
-//Application dependencies
+/**
+ * Application dependencies
+ */
 $container->add(\GuzzleHttp\Client::class);
 
-//Application components
+/**
+ * Core components
+ */
 $container->add(\PPBot\Request\SlackRequestFactory::class)->addArgument($config);
 
 $container->add(\PPBot\Consumer\SlackConsumer::class)
@@ -19,5 +23,12 @@ $container->add(\PPBot\Consumer\SlackConsumer::class)
     ->addArgument(\PPBot\Request\SlackRequestFactory::class);
 
 $container->add(\PPBot\Command\SendBookCommand::class)->addArgument(\PPBot\Consumer\SlackConsumer::class);
+
+$container->add(\Symfony\Component\Console\Application::class, function() use ($container) {
+    $app = new \Symfony\Component\Console\Application();
+    $app->add($container->get(\PPBot\Command\SendBookCommand::class));
+
+    return $app;
+});
 
 return $container;
